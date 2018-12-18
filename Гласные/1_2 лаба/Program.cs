f2bd3b97b8289c42d155c744e4c17508ee9fcc71
sasha4713 <sanya.barckov@yandex.ru>
2018-12-18 21:21:24 +0500

    задание про гласные

---
+++ Гласные/1_2 лаба/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("Введите строку: ");
+            string S = Console.ReadLine();
+            S = S.ToLower();
+
+            char[] C = { 'А', 'а', 'Е', 'е', 'Ё', 'ё', 'У', 'у', 'Ы', 'ы', 'Я', 'я', 'И', 'и', 'О', 'о', 'Ю', 'ю', 'Э', 'э' };
+            int i = 0;
+            
+            foreach (char d in S)
+            {
+                if (d != '!') 
+                {
+                    if (C.Contains(d))
+                    {
+                        i++;
+                    }
+                    
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Гласных букв в строке:{0}", i);
+                    Console.ReadLine();
+                }
+                
+            }
+            Console.WriteLine("Гласных букв в строке:{0}", i);
+            Console.ReadLine();
+
+        }
+          
+        
+        }
+    }
+
